37010286fecd5eac56d3850422f25634d7e2dc6d
Kevin Gomez <u20221203413@usco.edu.co>
2024-10-07 18:59:22 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id:'USD', nombre:'Dolar de Estados Unidos'},
+    { id:'COP', nombre:'Peso Colombiano'},
+    { id:'EUR', nombre:'Euro'},
+    { id:'MXN', nombre:'Peso Mexicano'},
+
+  ]
+  export {
+    monedas
+  }
